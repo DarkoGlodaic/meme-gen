85e174f1710f7906a78f0eb701628cca8bd477fd
Darko Glodaic <darko.glodjaic@gmail.com>
2025-02-07 15:16:42 +0100

    Updated CSS, styled Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import memeMan from "/meme_man.png"
 export default function Header() {
     return (
         <header className="header">
-            <img src={memeMan}
+            <img src={memeMan} alt="Meme Man"
             />
             <h1>Meme Gen</h1>
         </header>
