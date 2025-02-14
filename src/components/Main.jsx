9e91ab07eb3c7217a4d10d1518e9e5f63711fed4
Darko Glodaic <darko.glodjaic@gmail.com>
2025-02-14 13:50:42 +0100

    Updated to use state and added topText input functionality.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,31 @@
+import { useState } from 'react'
+
 export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: "Road work ahead?",
+        bottomText: "I sure hope it does",
+        imageUrl: "https://i.imgflip.com/7bv2j9.jpg?a483036"
+    })
+
+    function handleChange(event) {
+        const {value} = event.currentTarget
+        console.log(value)
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            topText: value
+            })
+        )
+    }
+
     return (
         <main>
             <div className="form">
                 <label>Top Text
                     <input
                         type="text"
-                        placeholder="Road work ahead"
+                        placeholder="Road work ahead?"
                         name="topText"
+                        onChange={handleChange}
                     />
                 </label>
 
@@ -20,9 +39,9 @@ export default function Main() {
                 <button>Get a new image 🖼</button>
             </div>
             <div className="meme">
-                <img src="https://i.imgflip.com/7bv2j9.jpg?a483036"/>
-                <span className="top">Road work ahead</span>
-                <span className="bottom">I sure hope it does</span>
+                <img src={meme.imageUrl}/>
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
